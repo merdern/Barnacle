70a8c0c65b9d3547441ee69ef3362dfb9b28570c
thai <twcy2205@gmail.com>
2023-05-09 16:21:49 +0100

    Update LiveData.py

--- pages/LiveData.py
+++ pages/LiveData.py
@@ -227,6 +227,7 @@ if ZerosFile is not None:
 
                 Barndata_with_newdata = Barndata.iloc[-1].copy()
                 Barndata_with_newdata = Barndata_with_newdata + newdata
+                st.write(Barndata_with_newdata)
 
                 denominator = np.mean(Barndata.iloc[:, 0:4], axis = 1)
                 probeyaw = (Barndata_with_newdata.iloc[1]-Barndata_with_newdata.iloc[3])/denominator
