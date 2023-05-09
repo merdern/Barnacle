ce209bd5f92af0c6005a6e328630ada1c3aa7781
thai <twcy2205@gmail.com>
2023-05-09 16:09:57 +0100

    Update LiveData.py

--- pages/LiveData.py
+++ pages/LiveData.py
@@ -236,14 +236,14 @@ if ZerosFile is not None:
 
                 
                 if yawcal2[65] > probeyaw.values[0] > yawcal2[25]:
-                    Barndata.loc[len(Barndata), [1,3]] = Barndata.iloc[-1, [1,3]] + newdata.iloc[0, [1,3]]
+                    Barndata.loc[len(Barndata), [1,3]] = Barndata.iloc[-1, [1,3]] + newdata.iloc[[1,3]]
                 else:
-                     Barndata.loc[len(Barndata), [1,3]] = Barndata.iloc[-1, [1,3]] - newdata.iloc[0, [1,3]]    
+                     Barndata.loc[len(Barndata), [1,3]] = Barndata.iloc[-1, [1,3]] - newdata.iloc[[1,3]]    
 
                 if yawcal2[65] > probepitch.values[0] > yawcal2[25]:
-                    Barndata.loc[len(Barndata), [0,2]] = Barndata.iloc[-1, [0,2]] + newdata.iloc[0, [0,2]]
+                    Barndata.loc[len(Barndata), [0,2]] = Barndata.iloc[-1, [0,2]] + newdata.iloc[[0,2]]
                 else:
-                     Barndata.loc[len(Barndata), [0,2]] = Barndata.iloc[-1, [0,2]] + newdata.iloc[0, [0,2]]       
+                     Barndata.loc[len(Barndata), [0,2]] = Barndata.iloc[-1, [0,2]] + newdata.iloc[[0,2]]       
                 
                 
                 st.write(Barndata)
