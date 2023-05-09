4484b5ecf4d66a9d04ce6f41f408e3b7187e04d2
thai <twcy2205@gmail.com>
2023-05-09 17:00:34 +0100

    Update LiveData.py

--- pages/LiveData.py
+++ pages/LiveData.py
@@ -259,6 +259,8 @@ if ZerosFile is not None:
                 
                 time.sleep(1/16)
 
+                st.write(Barndata)
+
                 if len(Barndata) != 0 and len(Barndata) % 16 == 0:
                     
                     
