36eb71543088aed0a2d40ee92599f7db0df2e4f8
thai <twcy2205@gmail.com>
2023-05-09 18:29:13 +0100

    Update Programming.py

--- pages/Programming.py
+++ pages/Programming.py
@@ -8,5 +8,7 @@ freq = st.number_input('Set Frequency of Data Logger (Hz)', value = 16)
 
 depth = st.number_input('Set Expected Depth of Barnacle (m)', value = 100)
 
+temp = st.number_input('Set Expected Temperature of Barnacle (°C)', value = 12)
+
 st.button("Program Barnacle", use_container_width=True)
 
